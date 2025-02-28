9f16f6e885deba127572ccf86ef0ee41197daab4
kenny-zheng <zokmi.zheng@gmail.com>
2025-02-28 12:47:39 +0800

    update model

---
+++ webAPI8/App.Model/EmployeeGetListArgs.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Model
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    namespace Northwind.Models
+    {
+        // 員工表格
+        public class EmployeeGetListArgs
+        {
+            /// <summary>
+            /// 員工ID
+            /// </summary>
+            public int EmployeeID { get; set; } 
+
+            /// <summary>
+            /// 姓氏
+            /// </summary>
+            public string LastName { get; set; } 
+
+            /// <summary>
+            /// 名字
+            /// </summary>
+            public string FirstName { get; set; }
+
+            /// <summary>
+            /// 職稱
+            /// </summary>
+            public string Title { get; set; } 
+
+            /// <summary>
+            /// 出生日期
+            /// </summary>
+            public DateTime? BirthDate { get; set; } 
+
+            /// <summary>
+            /// 城市
+            /// </summary>
+            public string City { get; set; } 
+
+            /// <summary>
+            ///  國家
+            /// </summary>
+
+            public string Country { get; set; } 
+
+        }
+    }
+}
---
+++ webAPI8/App.Model/EmployeeRemoveDataArgs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Model
+{
+    public class EmployeeRemoveDataArgs
+    {
+        /// <summary>
+        /// 員工ID
+        /// </summary>
+        public int EmployeeID { get; set; }
+    }
+}
