bfb3c347d54dddd5c3fb5009ed0677f390027b53
kenny-zheng <zokmi.zheng@gmail.com>
2025-02-28 14:18:39 +0800

    DB專案

--- webAPI8/App.DB/dbo/Tables/tblUserOnGroup.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[tblUserOnGroup] (
-    [cUserID]      INT NOT NULL,
-    [cUserGroupID] INT NOT NULL,
-    [cStatus]      BIT NULL,
-    CONSTRAINT [PK_tblUserOnGroup] PRIMARY KEY CLUSTERED ([cUserID] ASC, [cUserGroupID] ASC)
-);
-
